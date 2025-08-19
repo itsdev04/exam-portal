d35b7cea1a82f2c6605ac2d3e566798aaf09d24e
itsdev04 <imkrdev@gmail.com>
2025-08-20 00:59:05 +0530

    signup-form data-binding

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { Component, OnInit } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, NgForm } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
-  imports: [MatSelectModule,MatInputModule,MatFormFieldModule],
+  standalone: true,
+  imports: [MatSelectModule, MatInputModule, MatFormFieldModule, FormsModule, JsonPipe],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+
+  public user = {
+    username: '',
+    password: '',
+    firstname: '',
+    lastname: '',
+    email: '',
+    phonenumber: ''
+  }
+  ngOnInit(): void {
+  }
+
+  formSubmit(registerForm: NgForm): any {
+    console.log("Form submitted ", registerForm.value);
+    console.log(JSON.stringify(registerForm.value));
+  }
+
+  handleRegsister(): any {
+    console.log("Clicked on Register")
+  }
 
 }
